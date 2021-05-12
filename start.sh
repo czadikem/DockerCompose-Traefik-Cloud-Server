53b10386b6ba8eecb50d63d4c75e91dea8a76ba6
czadikem <35937408+czadikem@users.noreply.github.com>
2021-05-12 16:48:52 -0600

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+docker-compose -f ~/traefik/traefik-configurations/docker-compose.yml up -d
+docker-compose -f ~/traefik/apps/jitsi/docker-compose.yml up -d
+docker-compose -f ~/traefik/apps/mailu/docker-compose.yml up -d
+docker-compose -f ~/traefik/apps/rocketchat/docker-compose.yml up -d
+docker-compose -f ~/traefik/apps/searx/docker-compose.yml up -d
+
+
+
+
