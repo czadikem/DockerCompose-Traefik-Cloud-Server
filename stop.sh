9607432a9e0516fdd4e1f061121c6e81eba2cc9b
czadikem <35937408+czadikem@users.noreply.github.com>
2021-05-12 16:51:03 -0600

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-docker-compose -f ~/traefik/traefik-configurations/docker-compose.yml down
-docker-compose -f ~/traefik/apps/jitsi/docker-compose.yml down
-docker-compose -f ~/traefik/apps/mailu/docker-compose.yml down
-docker-compose -f ~/traefik/apps/rocketchat/docker-compose.yml down
-docker-compose -f ~/traefik/apps/searx/docker-compose.yml down
+docker-compose -f traefik/traefik-configurations/docker-compose.yml down
+docker-compose -f traefik/apps/jitsi/docker-compose.yml down
+docker-compose -f traefik/apps/mailu/docker-compose.yml down
+docker-compose -f traefik/apps/rocketchat/docker-compose.yml down
+docker-compose -f traefik/apps/searx/docker-compose.yml down
 
 
 
